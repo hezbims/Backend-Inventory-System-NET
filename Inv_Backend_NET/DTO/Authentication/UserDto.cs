17a931650512c610a8afd874b9775ddb850fc610
hezbims <hezbisulaiman@gmail.com>
2023-12-07 11:45:27 +0800

    user dto sekarang ngereturn isAdmin

--- Inv_Backend_NET/DTO/Authentication/UserDto.cs
+++ Inv_Backend_NET/DTO/Authentication/UserDto.cs
@@ -8,17 +8,21 @@ public class UserDto
     [JsonPropertyName("id")] public int Id { get; set; }
     [JsonPropertyName("username")] public string Username { get; set; }
     
-    public UserDto(int id, string username)
+    [JsonPropertyName("is_admin")] public bool IsAdmin { get; set; }
+    
+    public UserDto(int id, string username, bool isAdmin)
     {
         Id = id;
         Username = username;
+        IsAdmin = isAdmin;
     }
 
     public static UserDto From(User user)
     {
         return new UserDto(
             id: user.Id,
-            username: user.Username
+            username: user.Username,
+            isAdmin: user.IsAdmin
         );
     }
 }
