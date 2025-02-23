fae645036216ae71c3d3fbcd51c95a0331c9f0bd
hezbims <hezbisulaiman@gmail.com>
2025-02-23 13:13:56 +0800

    fix: update PaginatorExtension supaya possible support full page offset pagination

--- Inv_Backend_NET/Fitur/_Logic/Extension/PaginatorExtension.cs
+++ Inv_Backend_NET/Fitur/_Logic/Extension/PaginatorExtension.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using Inventory_Backend_NET.Fitur._Constants;
 
 namespace Inventory_Backend_NET.Fitur._Logic.Extension;
 
@@ -7,65 +6,78 @@ public static class PaginatorExtension
 {
     public static PaginatedResult<T> Paginate<T>(
         this IQueryable<T> query,
-        int pageNumber
+        int pageNumber,
+        int pageSize = 50
     )
     {
+        int total = query.Count();
+        
         var data = query
-            .Skip((pageNumber - 1) * MyConstants.PageSize)
-            .Take(MyConstants.PageSize + 1)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize + 1)
             .ToList();
         
         
         int totalData = data.Count;
-        bool? next = null;
-        if (totalData > MyConstants.PageSize)
+        int? prevPage = pageNumber == 1 ? null : pageNumber - 1;
+        int? nextPage = null;
+        if (totalData > pageSize)
         {
             data.RemoveAt(totalData - 1);
-            next = true;
+            nextPage = pageNumber + 1;
         }
 
-        return new PaginatedResult<T>(
-            data: data,
-            hasNext: next
-        );
+        return new PaginatedResult<T>
+        {
+            Data = data,
+            Links = new PaginatedResult<T>.LinkInfo
+            {
+                CurrentPage = pageNumber,
+                NextPage = nextPage,
+                PrevPage = prevPage,
+                TotalPage = total / pageSize + (total % pageSize == 0 ? 0 : 1),
+            }
+        };
     }
 }
 
 public class PaginatedResult<T>
 {
     [JsonPropertyName("data")] 
-    public List<T> Data { get; set; }
+    public required List<T> Data { get; init; }
 
     [JsonPropertyName("links")] 
-    public LinkInfo Links{ get; init; }
+    public required LinkInfo Links{ get; init; }
 
     public class LinkInfo
     {
-        [JsonPropertyName("next")] 
-        public bool? HasNext { get; init; }
+        [JsonPropertyName("next")]
+        public bool HasNext => NextPage != null;
 
-        public LinkInfo(bool? hasNext)
-        {
-            HasNext = hasNext;
-        }
-    }
-    
-    public PaginatedResult(
-        List<T> data,
-        bool? hasNext
-    )
-    {
-        Data = data;
-        Links = new LinkInfo(hasNext: hasNext);
+        [JsonPropertyName("current_page")]
+        public required int CurrentPage { get; init; }
+        [JsonPropertyName("next_page")]
+        public required int? NextPage { get; init; }
+        [JsonPropertyName("prev_page")]
+        public required int? PrevPage { get; init; }
+        [JsonPropertyName("total_page")]
+        public required int TotalPage { get; init; }
     }
     
     public PaginatedResult<TO> MapTo<TO>(
         Func<T , TO> mapper    
     )
     {
-        return new PaginatedResult<TO>(
-            data : Data.Select(mapper).ToList(),
-            hasNext: Links.HasNext
-        );
+        return new PaginatedResult<TO>
+        {
+            Data = Data.Select(mapper).ToList(),
+            Links = new PaginatedResult<TO>.LinkInfo
+            {
+                CurrentPage = Links.CurrentPage,
+                NextPage = Links.NextPage,
+                PrevPage = Links.PrevPage,
+                TotalPage = Links.TotalPage
+            }
+        };
     }
 }
