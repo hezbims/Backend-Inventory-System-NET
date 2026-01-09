fffec9dda51af3b11f34cc117b848613f265db00
hezbims <hezbisulaiman@gmail.com>
2026-01-09 12:31:43 +0800

    docs(Get Current User) : Open API Spec

--- Inv_Backend_NET/Fitur/Autentikasi/GetCurrentUser/GetCurrentUserController.cs
+++ Inv_Backend_NET/Fitur/Autentikasi/GetCurrentUser/GetCurrentUserController.cs
@@ -1,13 +1,13 @@
-using System.Security.Claims;
+using System.Text.Json.Serialization;
 using Inventory_Backend_NET.Database;
-using Inventory_Backend_NET.Fitur._Constants;
 using Inventory_Backend_NET.Fitur._Logic.Extension;
 using Inventory_Backend_NET.Fitur._Logic.Services;
 using Inventory_Backend_NET.Fitur.Autentikasi._Dto.Response;
-using Microsoft.AspNetCore.Authorization;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Filters;
 
-namespace Inventory_Backend_NET.Controllers.Authentication;
+namespace Inventory_Backend_NET.Fitur.Autentikasi.GetCurrentUser;
 
 [Route("api/get-current-user")]
 public class GetCurrentUserController : ControllerBase
@@ -26,15 +26,40 @@ public class GetCurrentUserController : ControllerBase
         _jwtTokenService = jwtTokenService;
     }
     [HttpGet]
-    public IActionResult GetCurrentUser()
+    [ProducesResponseType(typeof(GetCurrentUserSucceedResponse), StatusCodes.Status200OK)]
+    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(GetCurrentUserOkExample))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public ActionResult<GetCurrentUserSucceedResponse> GetCurrentUser()
     {
         var user = _db.GetCurrentUserFrom(_httpContextAccessor);
+        if (user is null)
+            return Unauthorized();
+            
         var userDto = GetUserDto.From(user);
         
-        return Ok(new
-        {
-            token = _jwtTokenService.GenerateNewToken(user),
-            user = userDto
-        });
+        return new GetCurrentUserSucceedResponse(
+            Token: _jwtTokenService.GenerateNewToken(user),
+            User: userDto);
+    }
+}
+
+public record GetCurrentUserSucceedResponse(
+    [property: JsonPropertyName("token")]
+    [UsedImplicitly]
+    string Token,
+ 
+    [property: JsonPropertyName("user")]
+    [UsedImplicitly]
+    GetUserDto User
+);
+
+file class GetCurrentUserOkExample : IExamplesProvider<GetCurrentUserSucceedResponse>
+{
+    public GetCurrentUserSucceedResponse GetExamples()
+    {
+        return new GetCurrentUserSucceedResponse(
+            Token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImFkbWluIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE3NjgyNzE0MDEsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTE1NCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJ9.H46pIQejekT9s-GuXsEauIqGjK2zu2vitYyWAhLRu8Y",
+            User: new GetUserDto(id: 1, username: "admin", isAdmin: true));
     }
 }
